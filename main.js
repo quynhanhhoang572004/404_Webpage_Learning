9896137641d6fe1053c279dd07a51c5e318ce45a
quynhanhhoang572004 <quynhanhhoang572004@gmail.com>
2024-07-25 01:11:08 +0700

    more animation

--- main.js
+++ main.js
@@ -33,3 +33,20 @@ const shadowHeader = () =>{
                     : header.classList.remove('shadow-header')
 }
 window.addEventListener('scroll', shadowHeader)
+
+// Gsap animation
+gsap.from('.home__img-1', {duration: 2, y:-100})
+
+let tl1 = gsap.timeline()
+    tl1.from('.home__img-2', {duration: 1, x:-400, y:-50, rotatio: 32, scale: .5})
+    .to('.home__img-2', {duration: 1,rotation:15, scale: 1.2})
+    .to('.home__img-2', {duration: 1,rotation:0, scale: 1})
+
+    let tl2 = gsap.timeline()
+    tl2.from('.home__img-3', {duration: 1, x:50, y:50, rotatio: 30})
+    .to('.home__img-3', {duration: 5,rotation:360})
+
+    gsap.from('.home__img-4', {duration: 5, y:-100, scale:1.3 })
+    gsap.from('.home__img-4', {duration: 3, y: 100})
+
+  
